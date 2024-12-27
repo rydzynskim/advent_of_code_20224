ec7e93b770240afa5292c495a113c3f72d917eee
Mitch Rydzynski <mitch.rydzynski@gmail.com>
2024-12-27 09:37:23 -0500

    day 21

--- solutions/21_2.py
+++ solutions/21_2.py
@@ -31,18 +31,6 @@ def is_safe(start, path, pad):
     return True
 
 
-def permute(arr):
-    if len(arr) == 0:
-        return [[]]
-    result = []
-    for i in range(len(arr)):
-        element = arr[i]
-        rest = arr[:i] + arr[i + 1 :]
-        for p in permute(rest):
-            result.append([element] + p)
-    return result
-
-
 def dict_len(dict):
     total = 0
     for key in dict:
@@ -50,44 +38,34 @@ def dict_len(dict):
     return total
 
 
-def shortest_path(start, end, pad, all):
+def shortest_path(start, end, pad):
     vert_diff = pad[end][0] - pad[start][0]
     vert_dir = "^" if vert_diff < 0 else "v"
     hor_diff = pad[end][1] - pad[start][1]
     hor_dir = "<" if hor_diff < 0 else ">"
-    moves = [vert_dir] * abs(vert_diff) + [hor_dir] * abs(hor_diff)
-    if not all:
-        return "".join(moves)
-    seen = set()
-    for path in permute(moves):
-        if is_safe(start, path, pad):
-            seen.add("".join(path))
-    return seen
+    hor_moves = [hor_dir] * abs(hor_diff) + [vert_dir] * abs(vert_diff)
+    vert_moves = [vert_dir] * abs(vert_diff) + [hor_dir] * abs(hor_diff)
+
+    # move horizontal first if we have to move left so we have to
+    # move less to generate the following sequence
+    if hor_dir == "<":
+        if is_safe(start, hor_moves, pad):
+            return "".join(hor_moves)
+        return "".join(vert_moves)
+    if is_safe(start, vert_moves, pad):
+        return "".join(vert_moves)
+    return "".join(hor_moves)
 
 
-def get_num_moves(sequence):
-    previous = "A"
-    possible_paths = []
-    for target in sequence:
-        possible_paths.append(shortest_path(previous, target, num_pad, True))
-        previous = target
-    possibilities = [""]
-    for path_set in possible_paths:
-        next = []
-        for path in path_set:
-            for possible in possibilities:
-                next.append(possible + path + "A")
-        possibilities = next
-    return possibilities
-
-
-def get_dir_moves(sequences):
+def get_moves(sequences, pad):
+    # notice that a bunch of the sequences are repeating so we can
+    # just keep a frequency map instead of iterating over everything
     next = {}
     for sequence in sequences:
         previous = "A"
         freq = sequences[sequence]
         for target in sequence:
-            temp = shortest_path(previous, target, dir_pad, False) + "A"
+            temp = shortest_path(previous, target, pad) + "A"
             if temp in next:
                 next[temp] += freq
             else:
@@ -97,14 +75,10 @@ def get_dir_moves(sequences):
 
 
 def solve(sequence):
-    min = float("inf")
-    for path in get_num_moves(sequence):
-        p = {path: 1}
-        for _ in range(25):
-            p = get_dir_moves(p)
-        if dict_len(p) < min:
-            min = dict_len(p)
-    return min
+    p = get_moves({sequence: 1}, num_pad)
+    for _ in range(25):
+        p = get_moves(p, dir_pad)
+    return dict_len(p)
 
 
 raw_lines = []
@@ -117,13 +91,6 @@ for raw_line in raw_lines:
 
 complexity = 0
 for sequence in sequences:
-    print(sequence, solve(sequence))
     complexity += solve(sequence) * int(sequence[:3])
 
 print(complexity)
-
-# too high
-# 233179012785224
-
-# too low
-# 93201284709054
